558b0b4e322a3f2926bdb8edd991e2611bee1e18
Scott Herold <sherold@mail.usf.edu>
2020-04-07 05:43:59 -0400

    Data validation added from Items get/post methods

--- code/app.py
+++ code/app.py
@@ -16,12 +16,25 @@ class Item(Resource):
     # create fucntions for each HTTP method you will allow for the
     # resource
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'item': None}, 404 # sends 404 error
+        # filter using lambda function
+        # filter can return whatever data type you please; 'next'
+        # returns a single item, but you can also use 'list' to return
+        # a list.
+        # NOTE: you can call 'next' multiple times for
+        # additional items. 'next' can cause an error if there are no
+        # items returned from the filter function, so you need to add
+        # the None argument for if no items are returned
+        item = next(filter(lambda x: x['name'] == name, items), None)
+
+        # ternary if statement for if item is returned, or None (from
+        # filter)
+        return {'item': item}, 200 if item else 404
 
     def post(self, name):
+        # similar lambda from get; validates that there is no item with
+        # the requested name
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {'messsage': "An item with name '{}' already exists".format(name)}, 400
         data = request.get_json()
         item = {'name': name, 'price': data['price']}
         items.append(item)
@@ -39,4 +52,4 @@ api.add_resource(Item, '/item/<string:name>') # http://127.0.0.1:5000/item/chees
 api.add_resource(ItemList, '/items') # http//127.0.0.1:5000/items
 
 # ===== Server =====
-app.run(port=5000)
+app.run(port=5000, debug=True)
